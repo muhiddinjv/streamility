e1c751f11a0c1a0ad7cc79e14560305860821f9c
Muhiddin <muhiddinjv@gmail.com>
2022-05-26 05:15:18 +0500

    add semantic ui & connect the header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './streams/StreamEdit';
-import StreamDelete from './streams/StreamDelete';
-import StreamList from './streams/StreamList';
-import StreamShow from './streams/StreamShow';
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from './Header'
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<StreamList />} />
-        <Route path="/streams/new" exact element={<StreamCreate />} />
-        <Route path="/streams/edit" exact element={<StreamEdit />} />
-        <Route path="/streams/delete" exact element={<StreamDelete />} />
-        <Route path="/streams/show" exact element={<StreamShow />} />
-      </Routes>
-    </Router>
+    <div className="ui container">
+      
+      <Router>
+      <Header/>
+        <Routes>
+          <Route path="/" exact element={<StreamList />} />
+          <Route path="/streams/new" exact element={<StreamCreate />} />
+          <Route path="/streams/edit" exact element={<StreamEdit />} />
+          <Route path="/streams/delete" exact element={<StreamDelete />} />
+          <Route path="/streams/show" exact element={<StreamShow />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,18 @@
-import React from 'react'
-import App from './components/App';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import App from "./components/App";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App tab="home" />);
+
+/*  
+    ALT + I/J/K/L: Move cursor up/left/down/right
+    ALT + SHIFT + I/J/K/L: Mark lines/characters while moving character up/left/down/right
+    ALT + CTRL + J/L: Move cursor to start/end of word
+    SHIFT + CTRL + I/K: Move marked- or current line up/down
+    CTRL + J/L: Move cursor to start/end of line
+    ALT + SHIFT + O: Mark characters from cursor to end of line
+    ALT + SHIFT + U: Mark characters from cursor to start of line
+    CTRL + I/K: Add cursor on line above/below the current cursor position
+*/
