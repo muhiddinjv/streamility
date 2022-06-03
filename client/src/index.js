b5453c92b292644135eb90a82d5b69444399d0d1
Muhiddin <muhiddinjv@gmail.com>
2022-06-03 21:00:53 +0500

    feat: add redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import App from "./components/App";
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import{ legacy_createStore as createStore}  from 'redux';
+import{ legacy_createStore as createStore, applyMiddleware, compose }  from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
